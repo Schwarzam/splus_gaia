651f61a590d6bfff78d2ba212e81b16d2e2616dc
marinaizb <marina.izabela.mi@gmail.com>
2026-01-09 19:57:11 -0300

    dowload all mc fields and bica full catalog

---
+++ notebooks/download_mc.py
@@ -0,0 +1,19 @@
+import splusdata
+
+outdir = '/mnt/hdcasa/splus_gaia/mc_catalogs/'
+
+conn = splusdata.Core()
+
+
+for i in range(1, 170):
+    print(f'Downloading MC{i:04d}')
+    try:
+        query = f"""
+                select id,ra,dec,mag_psf*,err_mag_psf* from idr6.idr6 where field = 'MC{i:04d}'
+            """
+        mc = conn.query(
+            query
+        )
+        mc.to_csv(f'{outdir}/MC{i:04d}.csv', index=False)
+    except Exception as e:
+        print(f'Error downloading MC{i:04d}: {e}')
